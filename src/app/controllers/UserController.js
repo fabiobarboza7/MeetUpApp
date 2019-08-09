c626c75fe4e830bbc57beb3aa7f0ce30cd645808
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-08-09 11:47:18 -0300

    some errors

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -44,9 +44,9 @@ class UserController {
         .when('oldPassword', (oldPassword, field) =>
           oldPassword ? field.required() : field
         ),
-        confirmPassword:: Yup.string().when('password', (password, field) =>
-          password ? field.required().oneOf([Yuf.ref('password')]) : field
-        )
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
     });
 
     if (!(await schema.isValid(req.body))) {
