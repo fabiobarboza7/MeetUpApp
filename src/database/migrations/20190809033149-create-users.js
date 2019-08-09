a03a1409991889787c481fc97d8770056a707a47
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-08-09 01:31:48 -0300

    database connection to models

--- src/database/migrations/20190809033149-create-users.js
+++ src/database/migrations/20190809033149-create-users.js
@@ -23,6 +23,16 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
